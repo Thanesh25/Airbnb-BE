71686dc5255d7fd8824ec3befdb39c05e8612bde
Thaneshwar <Thaneshwar.js@gmail.com>
2024-09-02 20:14:29 +0530

    second commit

--- index.js
+++ index.js
@@ -11,12 +11,18 @@ const imageDownloader = require('image-downloader')
 const generateToken = require("./Utils/Jwt-Uitls");
 const fs = require('fs')
 const PlaceModel = require('./Models/Places')
-// const path = require("path");
+const { log } = require("console");
+const Booking = require('./Models/Booking')
+// dotenv configuration
 dotenv.config()
 
- const bcryptSalt = bcrypt.genSaltSync(10);
+//bcrypt saltfactor
+const bcryptSalt = bcrypt.genSaltSync(10);
+
 const app = express()
-app.use('/Uploads',express.static(__dirname+'/Uploads'))
+
+app.use('/Uploads', express.static(__dirname + '/Uploads'))
+//Middlewares
 app.use(express.json());
 app.use(
   cors({
@@ -24,8 +30,10 @@ app.use(
     credentials: true,
   })
 );
+// Database connection function called 
 ConnectDB()
 
+// register api
   app.post("/register", async (req, res) => {
     const { name, email, password } = req.body;
 
@@ -41,76 +49,59 @@ ConnectDB()
     }
   });
 
-
-
+// login api
 
   app.post("/login", async (req, res) => {
-    try {
-      const { email, password } = req.body;
 
-      if (!email || !password) {
-        return res
-          .status(400)
-          .json({ message: "Email and password are required" });
-      }
-
-      const userDoc = await User.findOne({ email });
-
-      if (!userDoc) {
-        return res.status(422).json("Invalid credentials");
-      }
-
-      if (!userDoc.password) {
-        return res.status(500).json("User password not set.");
-      }
-
-      const isPasswordCorrect = bcrypt.compareSync(password, userDoc.password);
-
-      if (isPasswordCorrect) {
-        const token = generateToken(userDoc);
-     
-        res.status(200).json({ userDoc, token });
-      } else {
-        res.status(422).json("Invalid credentials");
-      }
-    } catch (error) {
-      console.error("Login error:", error.message);
-      res.status(500).json({ message: "Server error", error: error.message });
+    const { email, password } = req.body;
+    const userDoc = await User.findOne({ email });
+    if (userDoc && bcrypt.compareSync(password, userDoc.password)) {
+      const token = await  generateToken(userDoc);
+      console.log(token);
+      res.status(200).json({token,userDoc});
+    } else {
+      res.status(422).json("Invalid credentials");
     }
   });
 
 
-  // app.post("/login", async (req, res) => {
 
-  //   const { email, password } = req.body;
-  //   const userDoc = await User.findOne({ email });
-  //   if (userDoc && bcrypt.compareSync(password, userDoc.password)) {
-  //     const token =  generateToken(userDoc);
-  //     res.status(200).json({userDoc,token});
-  //   } else {
-  //     res.status(422).json("Invalid credentials");
-  //   }
-  // });
+app.get("/profile", async (req, res) => {
 
-
-//  app.get("/profile", (req, res) => {
+    const token =  req.headers.authorization?.split(" ")[1];
+    try {
+      const decode = jwt.verify(token, process.env.JWT_SECRET)
+      const userDoc = await User.findById(decode.id)
+      if (!userDoc) {
+        res.status(404).json("User not found");
+      } else {
+         res.status(200).json({userDoc})
+      }
+    } catch (error) {
+      res.status(500).json({message:"error msg form catch block"})
+    }
    
-//    const { token } = req.cookies.token;
-//    if (token) {
-//      jwt.verify(token, jwtSecret, {}, async (err, userData) => {
-//        if (err) throw err;
-//        const { name, email, _id } = await User.findById(userData.id);
-//        res.json({ name, email, _id });
-//      });
-//    } else {
-//      res.json(null);
-//    }
-//  });
+});
 
-  app.post("/logout", (req, res) => {
-    res.cookie("token", "").json(true);
-  });
-// console.log({__dirname});
+// logout api
+let tokenStore = [];
+
+app.post("/logout", (req, res) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Token required" });
+  }
+
+  // Invalidate the token (e.g., by removing it from the token store)
+  tokenStore = tokenStore.filter((storedToken) => storedToken !== token);
+
+  return res.status(200).json({ message: "Logout successful" });
+});
+
+  
+
+// upload by link api
 app.post("/upload-by-link", async (req, res) => {
   const { link } = req.body;
   const newName = 'Photo' + Date.now() + ".jpg";
@@ -122,6 +113,7 @@ app.post("/upload-by-link", async (req, res) => {
   res.json(newName);
 });
 
+//Upload by files api 
 const photoMiddleware = multer({ dest: 'Uploads/' });
 app.post('/upload', photoMiddleware.array('photos', 100), (req, res) => {
   const uploadedFiles =[]
@@ -134,70 +126,111 @@ app.post('/upload', photoMiddleware.array('photos', 100), (req, res) => {
     uploadedFiles.push(newPath.replace('Uploads/', ''));
   }
   res.json(uploadedFiles);
-  
 })
 
+// post places api
 app.post('/places', async(req, res) => {
-  
-    try {
-      // const { id } = req.userData;
-      const { title, address, addedPhotos, description, perks, extraInfo, checkIn, checkOut,price, maxGuests } = req.body;
-
-      const placeDoc = await PlaceModel.create({
-        title,
-        address,
-        photos: addedPhotos,
-        description,
-        perks,
-        extraInfo,
-        checkIn,
-        checkOut,
-        maxGuests,
-        price,
-    
-      });
+   const token = req.headers.authorization?.split(" ")[1];
+   const { title, address, addedPhotos, description, perks, extraInfo, checkIn, checkOut,price, maxGuests } = req.body;
+  try {
+       const decode = jwt.verify(token, process.env.JWT_SECRET);
+    const placeDoc = await PlaceModel.create({
+      owner: decode.id,
+      title,
+      address,
+      photos: addedPhotos,
+      description,
+      perks,
+      extraInfo,
+      checkIn,
+      checkOut,
+      maxGuests,
+      price,
+    });
 
       res.status(201).json(placeDoc);
     } catch (error) {
       res.status(500).json({ message: 'Internal server error' });
     }
-  
-})
-// get all places 
-app.get('/places', async(req, res) => {
-   
-    try {
-      const places = await PlaceModel.find();
-      res.status(200).json(places);
-    } catch (error) {
-      res.status(500).json({ message: 'Internal server error' });
-    }
-  
 })
 
-app.get('/places/:id', async(req, res) => {
-
+//  get places api
+app.get('/user-places', async(req, res) => {
+  const token = req.headers.authorization?.split(" ")[1];
   try {
-    const place = await PlaceModel.findById(req.params.id);
-    place
-      ? res.status(200).json(place)
-      : res.status(404).json({ message: "Place not found" });
+    const decode = jwt.verify(token, process.env.JWT_SECRET);
+    const { id } = decode
+    res.status(200).json( await PlaceModel.find({owner:id})  )
   } catch (error) {
-    res.status(500).json({ message: "Internal server error" });
-    
+    res.status(500).json({errorMsg:"error msg from catch block"})
   }
 })
 
-    //get places
-    // app.get('/places', async (req, res) => {
-    //   const { token } = req.token
-    //   jwt.verify(token, process.env.JWT_SECRET, {}, async (err, userData) => {
+// places  get id api
+app.get('/places/:id',  async(req, res) => {
+   const {id}=req.params
+res.json( await PlaceModel.findById(id))
 
-    //     const { id } = userData;
-    //     res.json(awaitPlaces.find({owner:id}));
-    //   })
+})
+// put places api 
+app.put('/places', async (req, res) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  const {id,title,address,addedPhotos,description,perks,extraInfo,checkIn,checkOut,price,maxGuests,} = req.body;
+  try {
+    const decode = jwt.verify(token, process.env.JWT_SECRET);
+    const placeDoc = await PlaceModel.findById(id);
+    console.log("decode",decode.id);
+    console.log("placeDoc",placeDoc.owner.toString());
+    if (decode.id === placeDoc.owner.toString()) {
+      placeDoc.set({title,address,photos: addedPhotos,description,perks,extraInfo,checkIn,checkOut,maxGuests,price,});
+      await placeDoc.save();
+      res.status(200).json('ok');
+    }
+  } catch (error) {
+    res.status(500).json("error msg from catch block")
+  }
+})
+// get places api 
+app.get('/places', async(req, res) => {
+  res.json ( await  PlaceModel.find())
+  
+})
 
-    // })
+
+app.post("/bookings", async (req, res) => {
+  const token = req.headers.authorization?.split(" ")[1];
+   const decode = jwt.verify(token, process.env.JWT_SECRET);
+  const { place, checkIn, checkOut, numberOfGuests, name, phone, price } =
+    req.body;
+ await Booking.create({
+    place,
+    checkIn,
+    checkOut,
+    numberOfGuests,
+    name,
+    phone,
+    price,
+    user: decode.id,
+  })
+    .then((doc) => {
+      res.json(doc);
+    })
+    .catch((err) => {
+      throw err;
+    });
+});
+
+app.get('/bookings', async(req, res) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  try {
+    const decode = jwt.verify(token, process.env.JWT_SECRET);
+    res.status(200).json(await Booking.find({ user: decode.id }).populate('place'));
+  } catch (error) {
+    res.status(500).json({message:"error msg from booking get api "})
+  }
+  
+})
+
 
     app.listen(process.env.PORT, () => {
       console.log(`Server is running on port ${process.env.PORT}`);
